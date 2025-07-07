a5c8bd68479d3353def8ad10f0c345c1e66f978a
Control Owl <control-owl@r-o0-t.wtf>
2025-07-07 22:42:40 +0200

    what-now-1

---
+++ docker/static_library/cargo-c.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+# authors = ["Control Owl <qr2m[at]r-o0-t[dot]wtf>"]
+# license = "CC-BY-NC-ND-4.0  [2023-2025]  Control Owl"
+#
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
+set -e
+set -x
+set -o pipefail
+
+CIRCUS="/home/QR2M/compile-circus"
+LOG_DIR="$CIRCUS/LOG"
+STATIC_DIR="$CIRCUS/STATIC"
+
+mkdir -p "$CIRCUS"
+mkdir -p "$LOG_DIR"
+mkdir -p "$STATIC_DIR"
+
+cd "$CIRCUS"
+
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
+{
+  cargo install cargo-c
+} 2>&1 | tee "$LOG_DIR/cargo-c-01-install.log"
+
+STATUS=${PIPESTATUS[0]}
+if [ "$STATUS" -ne 0 ]; then
+  cat $LOG_DIR/cargo-c-01-install.log
+  echo "ERROR - cargo-c - 01/02 - Install"
+  exit 1
+fi
+
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
+{
+  cargo-cbuild --version
+} 2>&1 | tee "$LOG_DIR/cargo-c-02-verify.log"
+
+STATUS=${PIPESTATUS[0]}
+if [ "$STATUS" -ne 0 ]; then
+  cat $LOG_DIR/cargo-c-01-install.log
+  echo "ERROR - cargo-c - 02/02 - Verify"
+  exit 1
+fi
+
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
+echo "cargo-c installed successfully"
