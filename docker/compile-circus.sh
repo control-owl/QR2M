fcd99315d03d48bac6ebba51ea8ccb0e9013bdaa
Marino Oddo <control-owl@r-o0-t.wtf>
2025-07-07 09:03:44 +0200

    almost-there-4

--- docker/compile-circus.sh
+++ docker/compile-circus.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+# authors = ["Control Owl <qr2m[at]r-o0-t[dot]wtf>"]
+# license = "CC-BY-NC-ND-4.0  [2023-2025]  Control Owl"
+#
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
 set -e
+set -x
+set -o pipefail
 
 APP_NAME="QR2M"
 TARGET="x86_64-unknown-linux-musl"
@@ -14,13 +21,15 @@ STATIC_DIR="$CIRCUS/STATIC"
 
 mkdir -p "$CIRCUS"
 mkdir -p "$LOG_DIR"
+mkdir -p "$STATIC_DIR"
 
 cd /home/QR2M/compile-circus
+
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
 
 echo "Set PKG_CONFIG_PATH"
 export PKG_CONFIG_PATH="$STATIC_DIR/lib:$STATIC_DIR/lib/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/x86_64-linux-musl/pkgconfig:/usr/local/lib/pkgconfig"
 echo "PKG_CONFIG_PATH=$PKG_CONFIG_PATH"
-
 
 echo "Set environment variables for build"
 export PKG_CONFIG_ALLOW_CROSS=1
@@ -31,47 +40,72 @@ export OPENSSL_LIB_DIR=/usr/lib
 export OPENSSL_INCLUDE_DIR=/usr/include
 export OPENSSL_STATIC=1
 export RUSTFLAGS="-C target-feature=+crt-static -C link-arg=-L/usr/lib -C link-arg=-L$STATIC_DIR/lib/pkgconfig -C link-arg=-L$STATIC_DIR/lib -C link-arg=-lssl -C link-arg=-lcrypto -C link-arg=-static"
-#export RUSTFLAGS="-C target-feature=+crt-static -C link-arg=-L$STATIC_DIR/lib -C link-arg=-static"
-
 
 echo "Checking pkg-config for dependencies"
 for pkg in gtk4 libadwaita-1; do
   echo "Checking $pkg..."
-  pkg-config --modversion "$pkg" 2>&1 | tee -a "$LOG_DIR/pkg_config_check.log" || { echo "Error: $pkg not found"; exit 1; }
+  {
+    pkg-config --modversion "$pkg"
+  } 2>&1 | tee "$LOG_DIR/qr2m-01-pkgconfig.log"
+
+  STATUS=${PIPESTATUS[0]}
+  if [ "$STATUS" -ne 0 ]; then
+    cat "$LOG_DIR/qr2m-01-pkgconfig.log"
+    echo "ERROR - qr2m - 01/05 - pkgconfig"
+    exit 1
+  fi
 done
 
 echo "Listing .pc files in $STATIC_DIR/lib/pkgconfig:"
-ls -l "$STATIC_DIR/lib/pkgconfig" | tee -a "$LOG_DIR/pkg_config_list.log"
+ls -l "$STATIC_DIR/lib/pkgconfig"
 
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
 
 echo "Cloning project..."
-git clone https://github.com/control-owl/QR2M QR2M
+{
+  git clone https://github.com/control-owl/QR2M QR2M
+} 2>&1 | tee "$LOG_DIR/qr2m-02-clone.log"
+
+STATUS=${PIPESTATUS[0]}
+if [ "$STATUS" -ne 0 ]; then
+  cat "$LOG_DIR/qr2m-02-clone.log"
+  echo "ERROR - qr2m - 02/05 - Clone"
+  exit 1
+fi
+
 cd QR2M
 
-cargo build --release --target "$TARGET" --features "$FEATURES" --locked -vv 2>&1 | tee "$LOG_DIR/qr2m_build.log"
-STATUS=$?
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
+{
+  cargo build --release --target "$TARGET" --features "$FEATURES" --locked -vv
+} 2>&1 | tee "$LOG_DIR/qr2m-03-build.log"
+
+STATUS=${PIPESTATUS[0]}
 if [ "$STATUS" -ne 0 ]; then
-  cat $LOG_DIR/qr2m_build.log
-  echo "CARGO BUILD QR2M FAIL"
+  cat $LOG_DIR/qr2m-03-build.log
+  echo "ERROR - qr2m - 03/05 - Build"
   exit 1
 fi
 
 echo "Listing build directory:"
 ls -l "$BUILD_PATH/release"
 
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
 
+{
+  cargo test --release --locked - --no-fail-fast --target "$TARGET" --features "$FEATURES"
+} 2>&1 | tee "$LOG_DIR/qr2m-04-test.log"
 
-# PAUSE
-#cargo test --release --locked - --no-fail-fast --target "$TARGET" --features "$FEATURES" 2>&1 | tee "$LOG_DIR/qr2m_test.log"
-#STATUS=$?
-#if [ "$STATUS" -ne 0 ]; then
-#  cat $LOG_DIR/qr2m_test.log
-#  echo "CARGO TEST QR2M FAIL"
-#  exit 1
-#fi
-#
-#echo "Listing build directory:"
-#ls -l "$BUILD_PATH/release"
+STATUS=${PIPESTATUS[0]}
+if [ "$STATUS" -ne 0 ]; then
+  cat $LOG_DIR/qr2m-04-test.log
+  echo "ERROR - qr2m - 04/05 - Test"
+ exit 1
+fi
+
+# -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
+
 #
 #echo "Checking binary:"
 #export BIN="$BUILD_PATH/release/$APP_NAME"
