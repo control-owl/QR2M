5dba9e2e4f94aa37134c675edc503dfb4077c43c
Marino Oddo <control-owl@r-o0-t.wtf>
2025-07-22 07:41:07 +0200

    Vlatko-29

--- docker/static_library/librsvg.sh
+++ docker/static_library/librsvg.sh
@@ -17,11 +17,11 @@ mkdir -p "$LOG_DIR"
 mkdir -p "$STATIC_DIR"
 
 export PKG_CONFIG_LIBDIR="/home/QR2M/compile-circus/STATIC/lib/pkgconfig"
-export PKG_CONFIG_PATH="/home/QR2M/compile-circus/STATIC/share/pkgconfig:/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/x86_64-linux-musl/pkgconfig:/usr/local/lib/pkgconfig"
+export PKG_CONFIG_PATH="/home/QR2M/compile-circus/STATIC/share/pkgconfig:/home/QR2M/compile-circus/STATIC/bin:/usr/lib/pkgconfig:/usr/share/pkgconfig:/usr/lib/x86_64-linux-musl/pkgconfig:/usr/local/lib/pkgconfig"
 export PKG_CONFIG="pkg-config --static"
 export CFLAGS="-I/home/QR2M/compile-circus/STATIC/include -O2 -fno-semantic-interposition -Wno-maybe-uninitialized"
 export LDFLAGS="-L/home/QR2M/compile-circus/STATIC/lib -lz -latomic"
-#export RUSTFLAGS="-C link-arg=-L/home/QR2M/compile-circus/STATIC/lib -C link-arg=-lz -C link-arg=-latomic"
+export RUSTFLAGS="-C link-arg=-L/home/QR2M/compile-circus/STATIC/lib -C link-arg=-lz -C link-arg=-latomic"
 export PATH="/home/QR2M/compile-circus/STATIC/bin:$PATH"
 export ZLIB_STATIC=1
 export PKG_CONFIG_zlib_STATIC="true"
@@ -62,7 +62,8 @@ fi
 # -.-. --- .--. -.-- .-. .. --. .... - / --.- .-. ..--- -- .- - .-. --- ----- - -.. --- - .-- - ..-.
 
 {
-  PATH="/home/QR2M/compile-circus/STATIC/bin:$PATH" ninja -C builddir
+  export PATH="/home/QR2M/compile-circus/STATIC/bin:$PATH"
+  ninja -C builddir
 } 2>&1 | tee "$LOG_DIR/librsvg-03-ninja.log"
 
 STATUS=${PIPESTATUS[0]}
