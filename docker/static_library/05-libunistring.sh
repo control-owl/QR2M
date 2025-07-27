0534c458dd077c84c710b1bddf5ae2fd65b01207
Control Owl <control-owl@r-o0-t.wtf>
2025-07-27 08:45:58 +0200

    Vlatko-51

--- docker/static_library/05-libunistring.sh
+++ docker/static_library/05-libunistring.sh
@@ -84,7 +84,8 @@ fi
   ./configure \
     --enable-static \
     --disable-shared \
-    --prefix=$STATIC_DIR
+    --prefix=$STATIC_DIR \
+    --with-gnulib-prefix=$STATIC_DIR
 } 2>&1 | tee -a "$LOG_FILE"
 
 STATUS=${PIPESTATUS[0]}
